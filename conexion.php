3b74ca344dc7b0319c623540044d3e4e00f0d8f7
DanielMunioz2 <dmunioz2034@gmail.com>
2024-08-16 17:30:01 -0400

    Actualizacion 7

--- conexion.php
+++ conexion.php
@@ -1,6 +1,7 @@
 <?php
 
-    $conn = new mysqli("localhost:3306", "root", "D@n!#l!ll01188390", "bizlab");
+    // $conn = new mysqli("localhost:3306", "root", "D@n!#l!ll01188390", "bizlab");
+    $conn = new mysqli("localhost", "bizlabadmin", "bizlabadminpass", "bizlabDB");
 
     if($conn->connect_error){
         die("Hubo un fallo en la conexión". $conn->$connect_error);
